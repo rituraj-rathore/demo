da410e97fdb25d899ee0f3bf1100192f3590f0cc
rituraj-rathore <47754150+rituraj-rathore@users.noreply.github.com>
2020-09-24 19:29:03 +0530

    coin tosh

---
+++ coin_tosh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+declare -A dictionary
+head=0
+tail=0
+for ((i=0;i<10;i++))
+do
+
+coin=$((RANDOM%2+1))
+
+if [ $coin -eq 1 ];
+then
+dictionary[$i]="head"
+head=$((head+1))
+else
+dictionary[$i]="tail"
+tail=$((tail+1))
+fi
+done
+echo " dictionary keys ${!dictionary[@]}"
+echo " dictionay value ${dictionary[@]}"
+echo "head count $head"
+echo "tail count $tail"
