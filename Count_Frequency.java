59ab4727f4f47ebb6570041038f6665727b5c9da
rituraj-rathore <47754150+rituraj-rathore@users.noreply.github.com>
2020-10-09 19:35:33 +0530

    stage 4 review

---
+++ Count_Frequency.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+ 
+public class Count_Frequency
+{   
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+         
+        //taking input
+         
+        System.out.println("Enter the number :");
+         
+        int inputNumber = sc.nextInt();
+         
+                 
+        HashMap<Integer, Integer> digitCountMap = new HashMap<Integer, Integer>();
+         
+        while (inputNumber != 0)
+        {
+           
+             
+            int lastDigit = inputNumber % 10;
+             
+            if(digitCountMap.containsKey(lastDigit))
+            {
+               
+                 
+                digitCountMap.put(lastDigit, digitCountMap.get(lastDigit)+1);
+            }
+            else
+            {
+                
+                digitCountMap.put(lastDigit, 1);
+            }
+             
+            //Removing lastdigit
+             
+            inputNumber = inputNumber / 10;
+        }
+         
+        //Printing digits frequency
+         
+        System.out.println(" ");
+         
+        System.out.println("Digits : Frequency");
+         
+        System.out.println("=");
+         
+        Set<Integer> keys = digitCountMap.keySet();
+         
+        for (Integer key : keys) 
+        {
+            System.out.println("   "+key+"   :   "+digitCountMap.get(key));
+        }
+         
+        sc.close();
+    }
+}
